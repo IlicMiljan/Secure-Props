33c09fc57b07d8fb46a1ba893b94d9112ebdbb05
Miljan Ilic <hello@miljanilic.com>
2024-03-09 23:44:29 +0100

    Add PSR-6 Cache Abstraction Wrapper

---
+++ src/Cache/Cache.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace IlicMiljan\SecureProps\Cache;
+
+use IlicMiljan\SecureProps\Reader\Exception\InvalidCacheKey;
+use Psr\Cache\CacheItemInterface;
+
+interface Cache
+{
+    /**
+     * Fetches a value from the cache or computes it if not found.
+     *
+     * @template T
+     *
+     * @param string   $key      The cache key.
+     * @param (callable(CacheItemInterface):T) $callable A callable that
+     *                           computes the value if it's not found in the
+     *                           cache.
+     * @param int|null $ttl      The time-to-live for the cache entry in seconds.
+     *
+     * @return T The cached or computed value.
+     * @throws InvalidCacheKey When $key is not valid.
+     */
+    public function get(string $key, callable $callable, ?int $ttl = null): mixed;
+}
---
+++ src/Cache/ItemPoolCompatibleCache.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace IlicMiljan\SecureProps\Cache;
+
+use IlicMiljan\SecureProps\Reader\Exception\InvalidCacheKey;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Cache\InvalidArgumentException;
+
+class ItemPoolCompatibleCache implements Cache
+{
+    private CacheItemPoolInterface $cachePool;
+
+    public function __construct(CacheItemPoolInterface $cachePool)
+    {
+        $this->cachePool = $cachePool;
+    }
+
+    public function get(string $key, callable $callable, ?int $ttl = null): mixed
+    {
+        try {
+            $cacheItem = $this->cachePool->getItem($key);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidCacheKey($key);
+        }
+
+        if (!$cacheItem->isHit()) {
+            $cacheItem->set($callable($cacheItem));
+            $cacheItem->expiresAfter($ttl);
+
+            $this->cachePool->save($cacheItem);
+        }
+
+        return $cacheItem->get();
+    }
+}
