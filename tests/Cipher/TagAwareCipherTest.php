0218b2c108a02c49d06008dfc53f94d5df54769f
Miljan Ilic <hello@miljanilic.com>
2024-03-15 15:36:17 +0100

    Improve Tag Aware Cipher Test

--- tests/Cipher/TagAwareCipherTest.php
+++ tests/Cipher/TagAwareCipherTest.php
@@ -20,19 +20,36 @@ class TagAwareCipherTest extends TestCase
      */
     private $encoder;
     private TagAwareCipher $cipher;
+    private TagAwareCipher $cipherWithCustomEncoder;
 
     protected function setUp(): void
     {
         $this->decoratedCipher = $this->createMock(Cipher::class);
         $this->encoder = $this->createMock(Encoder::class);
 
-        $this->cipher = new TagAwareCipher($this->decoratedCipher, $this->encoder);
+        $this->cipher = new TagAwareCipher($this->decoratedCipher);
+        $this->cipherWithCustomEncoder = new TagAwareCipher($this->decoratedCipher, $this->encoder);
     }
 
     /**
      * @throws CipherException
      */
     public function testEncrypt(): void
+    {
+        $this->decoratedCipher->expects($this->once())
+            ->method('encrypt')
+            ->with('plainText')
+            ->willReturn('encryptedText');
+
+        $result = $this->cipher->encrypt('plainText');
+
+        $this->assertEquals(base64_encode('<ENC>encryptedText</ENC>'), $result);
+    }
+
+    /**
+     * @throws CipherException
+     */
+    public function testEncryptWithCustomEncoder(): void
     {
         $encryptedText = 'encryptedText';
         $encodedText = 'encodedText';
@@ -48,7 +65,7 @@ class TagAwareCipherTest extends TestCase
             ->willReturn($encodedText);
 
 
-        $result = $this->cipher->encrypt('plainText');
+        $result = $this->cipherWithCustomEncoder->encrypt('plainText');
 
         $this->assertEquals($encodedText, $result);
     }
@@ -57,6 +74,26 @@ class TagAwareCipherTest extends TestCase
      * @throws CipherException
      */
     public function testDecryptWithEncryptedTag(): void
+    {
+        $encodedTextWithEncTags = base64_encode('<ENC>encodedTextWithTags</ENC>');
+        $decryptedText = 'plainText';
+
+        $this->decoratedCipher->expects($this->once())
+            ->method('decrypt')
+            ->with($this->equalTo('encodedTextWithTags'))
+            ->willReturn($decryptedText);
+
+        $result = $this->cipher->decrypt($encodedTextWithEncTags);
+
+        var_dump($result);
+
+        $this->assertEquals($decryptedText, $result);
+    }
+
+    /**
+     * @throws CipherException
+     */
+    public function testDecryptWithCustomEncoderAndEncryptedTag(): void
     {
         $encodedTextWithEncTags = 'encodedTextWithTags';
         $decryptedText = 'plainText';
@@ -71,21 +108,20 @@ class TagAwareCipherTest extends TestCase
             ->with($this->equalTo('encryptedText'))
             ->willReturn($decryptedText);
 
-        $result = $this->cipher->decrypt($encodedTextWithEncTags);
+        $result = $this->cipherWithCustomEncoder->decrypt($encodedTextWithEncTags);
 
         $this->assertEquals($decryptedText, $result);
     }
 
-
     /**
      * @throws CipherException
      */
-    public function testDecryptWithoutEncryptedTag(): void
+    public function testDecryptWithCustomEncoderAndWithoutEncryptedTag(): void
     {
         $this->encoder->expects($this->once())->method('decode');
         $this->decoratedCipher->expects($this->never())->method('decrypt');
 
-        $result = $this->cipher->decrypt('plainText');
+        $result = $this->cipherWithCustomEncoder->decrypt('plainText');
 
         $this->assertEquals('plainText', $result);
     }
