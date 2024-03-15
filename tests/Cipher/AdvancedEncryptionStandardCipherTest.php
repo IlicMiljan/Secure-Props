8a3683a7b1fc26949d5721aa8754257593764065
Miljan Ilic <hello@miljanilic.com>
2024-03-15 14:47:07 +0100

    Update Cipher Tests

--- tests/Cipher/AdvancedEncryptionStandardCipherTest.php
+++ tests/Cipher/AdvancedEncryptionStandardCipherTest.php
@@ -3,16 +3,31 @@
 namespace IlicMiljan\SecureProps\Tests\Cipher;
 
 use IlicMiljan\SecureProps\Cipher\AdvancedEncryptionStandardCipher;
+use IlicMiljan\SecureProps\Cipher\Exception\CipherException;
 use IlicMiljan\SecureProps\Cipher\Exception\InvalidKeyLength;
+use IlicMiljan\SecureProps\Encoder\Encoder;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class AdvancedEncryptionStandardCipherTest extends TestCase
 {
+    /** @var Encoder|MockObject */
+    private $encoder;
     private AdvancedEncryptionStandardCipher $cipher;
+    private AdvancedEncryptionStandardCipher $cipherWithCustomEncoder;
 
     protected function setUp(): void
     {
-        $this->cipher = new AdvancedEncryptionStandardCipher(openssl_random_pseudo_bytes(32));
+        $this->encoder = $this->createMock(Encoder::class);
+
+        $this->cipher = new AdvancedEncryptionStandardCipher(
+            openssl_random_pseudo_bytes(32),
+        );
+
+        $this->cipherWithCustomEncoder = new AdvancedEncryptionStandardCipher(
+            openssl_random_pseudo_bytes(32),
+            $this->encoder
+        );
     }
 
     public function testConstructWithInvalidKeyLengthThrowsException(): void
@@ -22,11 +37,33 @@ class AdvancedEncryptionStandardCipherTest extends TestCase
         new AdvancedEncryptionStandardCipher(openssl_random_pseudo_bytes(16));
     }
 
+    /**
+     * @throws CipherException
+     */
     public function testEncryptAndDecryptSuccessfully(): void
     {
         $encryptedString = $this->cipher->encrypt('plainText');
         $decryptedString = $this->cipher->decrypt($encryptedString);
 
         $this->assertEquals('plainText', $decryptedString);
+    }
+
+    /**
+     * @throws CipherException
+     */
+    public function testEncryptAndDecryptWithCustomEncoder(): void
+    {
+        $this->encoder->method('encode')->willReturnCallback(function ($data) {
+            return $data;
+        });
+
+        $this->encoder->method('decode')->willReturnCallback(function ($data) {
+            return $data;
+        });
+
+        $encryptedText = $this->cipherWithCustomEncoder->encrypt('plainText');
+        $decryptedText = $this->cipherWithCustomEncoder->decrypt($encryptedText);
+
+        $this->assertEquals('plainText', $decryptedText);
     }
 }
--- tests/Cipher/AsymmetricEncryptionCipherTest.php
+++ tests/Cipher/AsymmetricEncryptionCipherTest.php
@@ -3,15 +3,22 @@
 namespace IlicMiljan\SecureProps\Tests\Cipher;
 
 use IlicMiljan\SecureProps\Cipher\AsymmetricEncryptionCipher;
+use IlicMiljan\SecureProps\Encoder\Encoder;
 use OpenSSLAsymmetricKey;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class AsymmetricEncryptionCipherTest extends TestCase
 {
+    /** @var Encoder|MockObject */
+    private $encoder;
     private AsymmetricEncryptionCipher $cipher;
+    private AsymmetricEncryptionCipher $cipherWithCustomEncoder;
 
     protected function setUp(): void
     {
+        $this->encoder = $this->createMock(Encoder::class);
+
         /** @var OpenSSLAsymmetricKey $asymmetricKey */
         $asymmetricKey = openssl_pkey_new();
         /** @var string[] $asymmetricKeyDetails */
@@ -20,6 +27,12 @@ class AsymmetricEncryptionCipherTest extends TestCase
         openssl_pkey_export($asymmetricKey, $privateKey);
 
         $this->cipher = new AsymmetricEncryptionCipher($asymmetricKeyDetails['key'], $privateKey);
+
+        $this->cipherWithCustomEncoder = new AsymmetricEncryptionCipher(
+            $asymmetricKeyDetails['key'],
+            $privateKey,
+            $this->encoder
+        );
     }
 
     public function testEncryptAndDecryptSuccessfully(): void
@@ -28,5 +41,21 @@ class AsymmetricEncryptionCipherTest extends TestCase
         $decryptedString = $this->cipher->decrypt($encryptedString);
 
         $this->assertEquals('plainText', $decryptedString);
+    }
+
+    public function testEncryptAndDecryptWithCustomEncoder(): void
+    {
+        $this->encoder->method('encode')->willReturnCallback(function ($data) {
+            return $data;
+        });
+
+        $this->encoder->method('decode')->willReturnCallback(function ($data) {
+            return $data;
+        });
+
+        $encryptedText = $this->cipherWithCustomEncoder->encrypt('plainText');
+        $decryptedText = $this->cipherWithCustomEncoder->decrypt($encryptedText);
+
+        $this->assertEquals('plainText', $decryptedText);
     }
 }
